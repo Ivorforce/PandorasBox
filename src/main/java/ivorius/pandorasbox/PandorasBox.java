26251f79ce4d50a0d023a8c7ebb2684602363592
Lukas Tenbrink <lukastenbrink@googlemail.com>
2016-09-04 22:17:36 +0200

    Bump version to 2.1.4.

--- src/main/java/ivorius/pandorasbox/PandorasBox.java
+++ src/main/java/ivorius/pandorasbox/PandorasBox.java
@@ -45,7 +45,7 @@ public class PandorasBox
 {
     public static final String NAME = "Pandora's Box";
     public static final String MOD_ID = "pandorasbox";
-    public static final String VERSION = "2.1.3";
+    public static final String VERSION = "2.1.4";
 
     @Instance(value = MOD_ID)
     public static PandorasBox instance;
